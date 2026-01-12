e8de9940e68d6b275e3fe32c3b1a1b6d21ddb6e3
seo1a <seola9820@naver.com>
2026-01-13 07:16:02 +0900

    GA 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,24 @@ import Street from './pages/Street';
 import StreetBrand from './pages/StreetBrand';
 import StreetItem from './pages/StreetItem';
 import StreetSnap from './pages/StreetSnap';
-//import Preppy
 import './App.css'
 import './index.css';
+import ReactGA from "react-ga4";
+import PageTracker from './PageTracker';
+
+ReactGA.initialize(import.meta.env.VITE_GA_ID);
 
 function App() {
   const streetItems = data.스트릿.아이템;
   
   return (
-   <Router>
+    <Router>
+      <PageTracker />
       <Routes>
         <Route path="/" element={<Street />} />
         <Route path="/street/brand" element={<StreetBrand />} />
         <Route path="/street/item" element={<StreetItem items={streetItems}/>}/>
         <Route path="/street/snap" element={<StreetSnap />} />
-
-
       </Routes>
     </Router>
   )
